62f4d2a54b66699bce4c0370e29ac0f430f960ec
JJW <123@12.com>
2026-05-31 20:04:38 +0800

    add files

---
+++ do.sh
@@ -0,0 +1,20 @@
+
+git add  "./do.sh" "./git_batch_folder.py" "./.git/config" "./.git/description" "./.git/HEAD" "./.git/hooks/applypatch-msg.sample" "./.git/hooks/commit-msg.sample" "./.git/hooks/fsmonitor-watchman.sample" "./.git/hooks/post-update.sample" "./.git/hooks/pre-applypatch.sample" "./.git/hooks/pre-commit.sample" "./.git/hooks/pre-merge-commit.sample" "./.git/hooks/pre-push.sample" "./.git/hooks/pre-rebase.sample" "./.git/hooks/pre-receive.sample" "./.git/hooks/prepare-commit-msg.sample" "./.git/hooks/push-to-checkout.sample" "./.git/hooks/update.sample" "./.git/info/exclude" "./细胞技术高清教材/split.sh" "./细胞技术高清教材/siRNA转染/实验介绍.mp4" "./细胞技术高清教材/siRNA转染/实验操作.7z.00" "./细胞技术高清教材/siRNA转染/实验操作.7z.01" "./细胞技术高清教材/siRNA转染/实验操作.7z.02" "./细胞技术高清教材/siRNA转染/实验操作.7z.03" "./细胞技术高清教材/siRNA转染/实验操作.7z.04" "./细胞技术高清教材/siRNA转染/结果分析及疑问解答.mp4" "./细胞技术高清教材/TUNEL法检测细胞凋亡/TUNEL法检测细胞凋亡实验指导.pdf" "./细胞技术高清教材/TUNEL法检测细胞凋亡/实验介绍.mp4" "./细胞技术高清教材/TUNEL法检测细胞凋亡/实验前准备.mp4" "./细胞技术高清教材/TUNEL法检测细胞凋亡/实验操作.7z.00" "./细胞技术高清教材/TUNEL法检测细胞凋亡/实验操作.7z.01" "./细胞技术高清教材/TUNEL法检测细胞凋亡/实验操作.7z.02" 
+git commit -m 'add files'
+git push -u origin main
+
+git add  "./细胞技术高清教材/TUNEL法检测细胞凋亡/疑问与解答.mp4" "./细胞技术高清教材/TUNEL法检测细胞凋亡/结果分析.mp4" "./细胞技术高清教材/全骨髓法分离培养大鼠骨髓间充质干细胞/实验介绍.mp4" "./细胞技术高清教材/全骨髓法分离培养大鼠骨髓间充质干细胞/实验操作.7z.00" "./细胞技术高清教材/全骨髓法分离培养大鼠骨髓间充质干细胞/实验操作.7z.01" "./细胞技术高清教材/全骨髓法分离培养大鼠骨髓间充质干细胞/实验操作.7z.02" "./细胞技术高清教材/全骨髓法分离培养大鼠骨髓间充质干细胞/实验操作.7z.03" "./细胞技术高清教材/全骨髓法分离培养大鼠骨髓间充质干细胞/实验操作.7z.04" "./细胞技术高清教材/全骨髓法分离培养大鼠骨髓间充质干细胞/结果分析及疑问解答.mp4" "./细胞技术高清教材/组织块贴壁法培养大鼠平滑肌细胞/实验介绍.mp4" 
+git commit -m 'add files'
+git push -u origin main
+
+git add  "./细胞技术高清教材/组织块贴壁法培养大鼠平滑肌细胞/实验操作.7z.00" "./细胞技术高清教材/组织块贴壁法培养大鼠平滑肌细胞/实验操作.7z.01" "./细胞技术高清教材/组织块贴壁法培养大鼠平滑肌细胞/实验操作.7z.02" "./细胞技术高清教材/组织块贴壁法培养大鼠平滑肌细胞/实验操作.7z.03" "./细胞技术高清教材/组织块贴壁法培养大鼠平滑肌细胞/结果分析及疑问解答.mp4" "./细胞技术高清教材/细胞传代/实验介绍.mp4" "./细胞技术高清教材/细胞传代/实验操作.mp4" "./细胞技术高清教材/细胞传代/结果分析及疑问解答.mp4" "./细胞技术高清教材/细胞侵袭/实验介绍.mp4" "./细胞技术高清教材/细胞侵袭/实验操作.7z.00" 
+git commit -m 'add files'
+git push -u origin main
+
+git add  "./细胞技术高清教材/细胞侵袭/实验操作.7z.01" "./细胞技术高清教材/细胞侵袭/实验操作.7z.02" "./细胞技术高清教材/细胞侵袭/结果分析及疑问解答.mp4" "./细胞技术高清教材/细胞冻存/实验介绍.mp4" "./细胞技术高清教材/细胞冻存/实验操作.7z.00" "./细胞技术高清教材/细胞冻存/实验操作.7z.01" "./细胞技术高清教材/细胞冻存/实验操作.7z.02" "./细胞技术高清教材/细胞冻存/实验操作.7z.03" "./细胞技术高清教材/细胞冻存/实验操作.7z.04" "./细胞技术高清教材/细胞冻存/结果分析及疑问解答.mp4" "./细胞技术高清教材/细胞划痕/实验介绍.mp4" "./细胞技术高清教材/细胞划痕/实验操作.7z.00" "./细胞技术高清教材/细胞划痕/实验操作.7z.01" 
+git commit -m 'add files'
+git push -u origin main
+
+git add  "./细胞技术高清教材/细胞划痕/实验操作.7z.02" "./细胞技术高清教材/细胞划痕/实验操作.7z.03" "./细胞技术高清教材/细胞划痕/实验操作.7z.04" "./细胞技术高清教材/细胞划痕/结果分析及疑问解答.mp4" "./细胞技术高清教材/细胞复苏/结果分析及疑问解答.mp4" 
+git commit -m 'add files'
+git push -u origin main
